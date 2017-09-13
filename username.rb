d91beb81e3bee189140b8079aa37698e8d7ee704
Anthony-g2 <ant.greenwell2@gmail.com>
2017-09-13 06:59:12 -0700

    this is hard

--- username.rb
+++ username.rb
@@ -2,13 +2,47 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  if first.length == 0
+    p nil
+  elsif last.length == 0
+    p nil
+  else
+    first_name = first.delete(" ")
+    last_name = last.delete(" ")
+    first_init = first_name[0]
+    name_temp = first_init + last_name
+    name_v1 = name_temp.delete(" ").downcase
+    p name_v1
+  end
 end
 
-def format_year
-  nil
+def format_year(year)
+  stringy = year.to_s
+  if stringy.length > 4
+    p nil
+  elsif stringy.length < 4
+    p nil
+  else
+    p stringy[2..3]
+  end
 end
 
-def build_username
-  nil
+def build_username(first_name, last_name, birth_year, i)
+  user_type_prefix+format_name(first_name,last_name)+format_year(birth_year)
+end
+
+def check_privilege(i = 'user')
+  if i < 2
+    'seller'
+  elsif i.between?(2,3)
+    'manager'
+  elsif i >= 3
+    'admin'
+  elsif i < 1
+    'user'
+  end
+end
+
+def user_type_prefix
+  check_privilege(i) + '-'
 end
--- warmup.rb
+++ warmup.rb
@@ -1,17 +1,31 @@
 # Run `rspec spec/the_warmup_spec.rb` to get started.
 
 def say_hello
-  "hi"
+  p "hello"
+  # => 'hi'
 end
+p say_hello
 
 def scream(message)
-  message
+  p message.upcase + '!'
+    # => 'Message'
 end
+p scream('hello world')
 
-def first_char
-  "z"
+def first_char(word)
+  p word[0].downcase
+  # => 'w' for word
 end
+p first_char('Anthony')
 
-def polly_wanna
-  "crackercrackercracker"
+def polly_wanna(str)
+  p str * 3
+ # => 'crackercrackercracker'
 end
+p polly_wanna('cracker')
+
+def after_you
+  p 'no, after you'
+  # => 'no, after you'
+end
+p after_you
